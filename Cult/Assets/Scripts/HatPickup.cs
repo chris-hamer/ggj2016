1013f9f8acfc800d0fae414d6e56e64c1ca14ac9
Tad Cordle <tcordle1@umbc.edu>
2016-01-30 09:20:11 -0500

    Hat

---
+++ Cult/Assets/Scripts/HatPickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatPickup : MonoBehaviour {
+
+    public Sprite up, down, left, right;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            collider.gameObject.GetComponent<PlayerControls>().hasHat = true;
+            CharacterSprites sprites = collider.gameObject.GetComponent<CharacterSprites>();
+            sprites.up = up;
+            sprites.down = down;
+            sprites.left = left;
+            sprites.right = right;
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+}
