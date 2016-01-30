b030978f82026258f6d5b6c07e79c01642b002a9
Michael the Devastator <davism4@umbc.edu>
2016-01-30 00:02:31 -0500

    Added PathNode and PathWalk for defining movement paths. See comments in code.

---
+++ Cult/Assets/Scripts/PathNode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathNode : MonoBehaviour {
+
+    public float WaitDuration { get { return waitDuration; } }
+    public bool IsOccupied { get { return isOccupied; } }
+    public Vector2 Position { get { return position; } }
+
+    private bool isOccupied = false;
+    private float waitDuration = 0.0f;
+
+    private Vector2 position;
+    new private Transform transform;
+
+	void Start () {
+        transform = GetComponent<Transform>();
+        position = (Vector2)transform.position;
+    }
+	
+}
---
+++ Cult/Assets/Scripts/PathWalk.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathWalk : MonoBehaviour {
+
+    public float MinStoppingDistance = 0.1f; // Radius for when the person has reached this point
+    
+    public int IndexNextPathNode = 0; // Index of the next node in the path
+    public Transform[] ListPathNodeTransforms;
+    /* In the editor, define the path by drag-dropping the transforms of the path nodes
+        that are in the scene. The PathNode script objects are then stored, and the PathWalk script
+        will use those as locations of the path to walk.
+    */
+    public bool DoesPathLoop = false;
+
+    new private Transform transform;
+    new private Rigidbody2D rigidbody;
+
+    private PathNode[] pathNodeList = null;
+    private float speed = PlayerControls.SPEED; // Moves at same speed as the player
+    private float waitTimer = 0.0f;
+    private bool isWalking = true;
+
+	void Start ()
+    {
+        transform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        pathNodeList = new PathNode[ListPathNodeTransforms.Length];
+        for (int ii=0; ii<ListPathNodeTransforms.Length; ii++)
+        {
+            pathNodeList[ii] = ListPathNodeTransforms[ii].GetComponent<PathNode>();
+        }
+    }
+	
+	void Update () {
+        // Are we waiting at the current point?
+        if (waitTimer > 0.0f)
+        {
+            waitTimer -= Time.deltaTime;
+        }
+        else // We are moving between points.
+        {
+            Vector2 difference = pathNodeList[IndexNextPathNode].Position - rigidbody.position;
+            // Keep moving unless we reach the destination.
+            if (difference.magnitude > MinStoppingDistance)
+            {
+                rigidbody.MovePosition(rigidbody.position + difference.normalized * PlayerControls.SPEED);
+            }
+            else
+            {
+                waitTimer = pathNodeList[IndexNextPathNode].WaitDuration;
+                IndexNextPathNode++;
+                if (DoesPathLoop) // Not all paths loop
+                    IndexNextPathNode = IndexNextPathNode % pathNodeList.Length;
+            }
+        }        
+	}
+}
